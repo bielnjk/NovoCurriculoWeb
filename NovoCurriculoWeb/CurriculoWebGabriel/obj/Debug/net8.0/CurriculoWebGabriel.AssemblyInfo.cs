31dcc5180fb5c77a7583f787eab7b1d341ad9518
bielnjk <gabrielfbboaventura600@gmail.com>
2025-07-15 15:57:38 -0400

    segundo commit

--- NovoCurriculoWeb/CurriculoWebGabriel/obj/Debug/net8.0/CurriculoWebGabriel.AssemblyInfo.cs
+++ NovoCurriculoWeb/CurriculoWebGabriel/obj/Debug/net8.0/CurriculoWebGabriel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CurriculoWebGabriel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc0517263e5360e3d9c8f0ec87433151ecf0331e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CurriculoWebGabriel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CurriculoWebGabriel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
